8d4d1c29606d0392eb450438a297b040f95284e1
k3csk3 <szilagyi.viktor89@gmail.com>
2023-02-11 18:23:31 +0100

    basics

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutusComponent } from './page/aboutus/aboutus.component';
+import { HomeComponent } from './page/home/home.component';
+import { LoginComponent } from './page/login/login.component';
+import { StoreComponent } from './page/store/store.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'aboutus',
+    component: AboutusComponent,
+  },
+  {
+    path: 'store',
+    component: StoreComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './common/navbar/navbar.component';
+import { HomeComponent } from './page/home/home.component';
+import { StoreComponent } from './page/store/store.component';
+import { AboutusComponent } from './page/aboutus/aboutus.component';
+import { LoginComponent } from './page/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    StoreComponent,
+    AboutusComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
